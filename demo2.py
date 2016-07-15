fe2c523241625c158d9f5be65146014f860d5bc9
Mathias Bynke <mabynke@1337.no>
2016-07-15 12:42:13 +0200

    Versjon fra GPU-maskinen

--- demo2.py
+++ demo2.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------
+# Faster R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+"""
+Demo script showing detections in sample images.
+
+See README.md for installation instructions before running.
+"""
+
 import _init_paths
 from fast_rcnn.config import cfg
 from fast_rcnn.test import im_detect
@@ -10,7 +25,7 @@ import caffe, os, sys, cv2
 import argparse
 
 CLASSES = ('__background__',
-           'aeroplane', 'bicycle', 'bird', 'boat',
+           'aeroplane', 'bicycle'), 'bird', 'boat'),
            'bottle', 'bus', 'car', 'cat', 'chair',
            'cow', 'diningtable', 'dog', 'horse',
            'motorbike', 'person', 'pottedplant',
@@ -19,34 +34,20 @@ CLASSES = ('__background__',
 NETS = {'vgg16': ('VGG16',
                   'VGG16_faster_rcnn_final.caffemodel'),
         'zf': ('ZF',
-                  'ZF_faster_rcnn_final.caffemodel')}
+                  'ZF_faster_rcnn_final.caffemodel'),
+        'egen': ('FFINett01',
+                  'FFINett01_faster_rcnn_final.caffemodel')
+        }
 
-def int_to_col(number):
 
-    switcher = {
-        0: 'gold',
-        1: 'deeppink',
-        2: 'dodgerblue',
-        3: 'lawngreen',
-        4: 'navy',
-        5: 'crimson',
-        6: 'darksalmon',
-        7: 'violet',
-        8: 'teal',
-        9: 'orange'
-    }
-
-    print number % len(switcher)
-    return switcher.get(number % len(switcher), "red")
-
-def vis_detections(im, class_name, cls_ind, dets, ax, thresh=0.5):
+def vis_detections(im, class_name, dets, thresh=0.5):
     """Draw detected bounding boxes."""
     inds = np.where(dets[:, -1] >= thresh)[0]
     if len(inds) == 0:
         return
 
     im = im[:, :, (2, 1, 0)]
-
+    fig, ax = plt.subplots(figsize=(12, 12))
     ax.imshow(im, aspect='equal')
     for i in inds:
         bbox = dets[i, :4]
@@ -56,20 +57,26 @@ def vis_detections(im, class_name, cls_ind, dets, ax, thresh=0.5):
             plt.Rectangle((bbox[0], bbox[1]),
                           bbox[2] - bbox[0],
                           bbox[3] - bbox[1], fill=False,
-                          edgecolor=int_to_col(cls_ind), linewidth=3.5, alpha=0.8)
+                          edgecolor='red', linewidth=3.5)
             )
-        ax.text(bbox[0] + 1, bbox[1] ,
+        ax.text(bbox[0], bbox[1] - 2,
                 '{:s} {:.3f}'.format(class_name, score),
-                bbox=dict(facecolor=int_to_col(cls_ind), alpha=0.6),
-                fontsize=14, color='snow')
+                bbox=dict(facecolor='blue', alpha=0.5),
+                fontsize=14, color='white')
 
+    ax.set_title(('{} detections with '
+                  'p({} | box) >= {:.1f}').format(class_name, class_name,
+                                                  thresh),
+                  fontsize=14)
+    plt.axis('off')
+    plt.tight_layout()
+    plt.draw()
 
-
-def demo(net, image_name):
+def demo(net, mappe, image_name):
     """Detect object classes in an image using pre-computed object proposals."""
 
     # Load the demo image
-    im_file = os.path.join(cfg.DATA_DIR, 'demo', image_name)
+    im_file = os.path.join(mappe, image_name)
     im = cv2.imread(im_file)
 
     # Detect all object classes and regress object bounds
@@ -81,65 +88,73 @@ def demo(net, image_name):
            '{:d} object proposals').format(timer.total_time, boxes.shape[0])
 
     # Visualize detections for each class
-    CONF_THRESH = 0.7
+    CONF_THRESH = 0.4
     NMS_THRESH = 0.3
-
-    fig, ax = plt.subplots(figsize=(12, 12))
-
     for cls_ind, cls in enumerate(CLASSES[1:]):
-
-        cls_ind += 1  # because we skipped background
+        cls_ind += 1 # because we skipped background
         cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
         cls_scores = scores[:, cls_ind]
         dets = np.hstack((cls_boxes,
                           cls_scores[:, np.newaxis])).astype(np.float32)
         keep = nms(dets, NMS_THRESH)
         dets = dets[keep, :]
+        vis_detections(im, cls, dets, thresh=CONF_THRESH)
 
-        vis_detections(im, cls, cls_ind, dets, ax, thresh=CONF_THRESH)
+def parse_args():
+    """Parse input arguments."""
+    parser = argparse.ArgumentParser(description='Faster R-CNN demo')
+    parser.add_argument('--gpu', dest='gpu_id', help='GPU device id to use [0]',
+                        default=0, type=int)
+    parser.add_argument('--cpu', dest='cpu_mode',
+                        help='Use CPU mode (overrides --gpu)',
+                        action='store_true')
+    parser.add_argument('--net', dest='demo_net', help='Network to use [vgg16]',
+                        choices=NETS.keys(), default='vgg16')
 
+    args = parser.parse_args()
 
-    ax.set_title(('{} detection with '
-              'p({} | box) >= {:.1f}').format('Object', 'obj',
-                                              CONF_THRESH),
-             fontsize=14)
-    plt.axis('off')
-    plt.tight_layout()
-    plt.draw()
-
+    return args
 
 if __name__ == '__main__':
     cfg.TEST.HAS_RPN = True  # Use RPN for proposals
 
+    args = parse_args()
+    print(args)
 
-    prototxt = os.path.join(cfg.MODELS_DIR, NETS['zf'][0],
+    prototxt = os.path.join(cfg.MODELS_DIR, NETS[args.demo_net][0],
                             'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
-
     caffemodel = os.path.join(cfg.DATA_DIR, 'faster_rcnn_models',
-                              NETS['zf'][1])
+                              NETS[args.demo_net][1])
 
     if not os.path.isfile(caffemodel):
         raise IOError(('{:s} not found.\nDid you run ./data/script/'
                        'fetch_faster_rcnn_models.sh?').format(caffemodel))
 
-    caffe.set_mode_cpu()
-
+    if args.cpu_mode:
+        caffe.set_mode_cpu()
+    else:
+        caffe.set_mode_gpu()
+        caffe.set_device(args.gpu_id)
+        cfg.GPU_ID = args.gpu_id
     net = caffe.Net(prototxt, caffemodel, caffe.TEST)
 
     print '\n\nLoaded network {:s}'.format(caffemodel)
 
     # Warmup on a dummy image
-    #im = 128 * np.ones((300, 500, 3), dtype=np.uint8)
-    #for i in xrange(2):
-    #    _, _= im_detect(net, im)
+    im = 128 * np.ones((300, 500, 3), dtype=np.uint8)
+    for i in xrange(2):
+        _, _= im_detect(net, im)
 
-    im_names = ['000456.jpg', '000542.jpg', '001150.jpg',
-                '001763.jpg', '004545.jpg']
-
-
+    #mappe = "/home/sommerstudent/py-faster-rcnn/data/demo" #Mappen med bildene som skal bli analysert
+    mappe = "/home/sommerstudent/testdata/10m"
+    im_names = os.listdir(mappe)
+    i = 0
     for im_name in im_names:
         print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-        print 'Demo for data/demo/{}'.format(im_name)
-        demo(net, im_name)
+        print 'Demo for {}'.format(os.path.join(mappe, im_name))
+        demo(net, mappe, im_name)
+        if i >= 50: # i >= n: Analyser kun n bilder
+            break
+        i += 1
 
     plt.show()
